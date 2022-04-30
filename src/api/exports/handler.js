05b90a2a8819020caed8acc5081b599b8d02ebcf
mrizkitriyanto <muhamadrizkitriyanto41@gmail.com>
2022-04-30 15:05:30 +0700

    Edit Error Handler

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -1,4 +1,3 @@
-const ClientError = require('../../exceptions/ClientError');
 
 class ExportsHandler {
   constructor(service, playlistsService, validator) {
@@ -11,53 +10,33 @@ class ExportsHandler {
   }
 
   async postExportPlaylistsHandler(request, h) {
-    try {
-      this._validator.validateExportPlaylistsPayload(request.payload);
+    this._validator.validateExportPlaylistsPayload(request.payload);
 
-      const userId = request.auth.credentials.id;
-      const playlistId = request.params.playlistid;
+    const userId = request.auth.credentials.id;
+    const playlistId = request.params.playlistid;
 
-      // Playlist check
-      await this._playlistsService.getPlaylistDetails(playlistId);
+    // Playlist check
+    await this._playlistsService.getPlaylistDetails(playlistId);
 
-      // Verify playlist owner
-      await this._playlistsService.verifyPlaylistOwner(playlistId, userId);
+    // Verify playlist owner
+    await this._playlistsService.verifyPlaylistOwner(playlistId, userId);
 
-      const message = {
-        playlistId,
-        targetEmail: request.payload.targetEmail,
-      };
+    const message = {
+      playlistId,
+      targetEmail: request.payload.targetEmail,
+    };
 
-      await this._service.sendMessage(
-          'export:playlists',
-          JSON.stringify(message),
-      );
+    await this._service.sendMessage(
+        'export:playlists',
+        JSON.stringify(message),
+    );
 
-      const response = h.response({
-        status: 'success',
-        message: 'Permintaan Anda dalam antrean',
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-
-      // Server ERROR!
-      const response = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda dalam antrean',
+    });
+    response.code(201);
+    return response;
   }
 }
 
